ca8c9c3f617a2cc7570c66974a5e7c86cbe95824
VIJITHkkk <vijithk486@gmail.com>
2026-07-03 19:13:01 +0530

    Integrate Firebase authentication for login

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-// Login Button
-const loginBtn = document.getElementById("loginBtn");
 
-if (loginBtn) {
-    loginBtn.addEventListener("click", function () {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1KKJiGC-x8tVf7eLn-mX3jXjjdoq58n0",
+  authDomain: "bsr-fish-stall-2b1d4.firebaseapp.com",
+  projectId: "bsr-fish-stall-2b1d4",
+  storageBucket: "bsr-fish-stall-2b1d4.firebasestorage.app",
+  messagingSenderId: "167357195775",
+  appId: "1:167357195775:web:38c1e500d38743b240b3ca"
+};
 
-        if (email === "" || password === "") {
-            alert("Please enter Email and Password");
-            return;
-        }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-        // Temporary Login
-        if (email === "admin@bsr.com" && password === "admin123") {
-            alert("Login Successful");
-            window.location.href = "admin.html";
-        } else {
-            alert("Invalid Email or Password");
-        }
+// Login
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Login Successful");
+      window.location.href = "admin.html";
+    })
+    .catch((error) => {
+      document.getElementById("message").innerText = error.message;
     });
-}
-
-// Add Fish Button
-const addFish = document.getElementById("addFish");
-
-if (addFish) {
-    addFish.addEventListener("click", function () {
-
-        const name = document.getElementById("fishName").value;
-        const price = document.getElementById("fishPrice").value;
-        const stock = document.getElementById("fishStock").value;
-
-        if (name === "" || price === "" || stock === "") {
-            alert("Fill all fields");
-            return;
-        }
-
-        alert("Fish Added Successfully");
-
-        document.getElementById("fishName").value = "";
-        document.getElementById("fishPrice").value = "";
-        document.getElementById("fishStock").value = "";
-    });
-}
+});
