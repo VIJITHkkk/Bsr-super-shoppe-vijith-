df8f5c5da7c89caf1d19007f3fdf35fade748624
VIJITHkkk <vijithk486@gmail.com>
2026-07-11 01:34:20 +0530

    Refactor intro video handling and remove Firebase auth
    Removed Firebase authentication and configuration code, added intro video controls.

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
+// Intro Video
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+const intro = document.getElementById("intro");
+const mainWebsite = document.getElementById("mainWebsite");
+const introVideo = document.getElementById("introVideo");
+const skipIntro = document.getElementById("skipIntro");
 
-// Firebase Configuration
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB1KKJiGC-x8tVf7eLn-mX3jXjjdoq58n0",
-  authDomain: "bsr-fish-stall-2b1d4.firebaseapp.com",
-  databaseURL: "https://bsr-fish-stall-2b1d4-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bsr-fish-stall-2b1d4",
-  storageBucket: "bsr-fish-stall-2b1d4.firebasestorage.app",
-  messagingSenderId: "167357195775",
-  appId: "1:167357195775:web:38c1e500d38743b240b3ca",
-  measurementId: "G-T9WDYZSWVX"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Login
-document.getElementById("loginBtn").addEventListener("click", () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      alert("Login Successful");
-      window.location.href = "./admin.html";
-    })
-    .catch((error) => {
-      document.getElementById("message").innerText = error.message;
-    });
+// Show website after video ends
+introVideo.addEventListener("ended", () => {
+    intro.style.display = "none";
+    mainWebsite.style.display = "block";
 });
+
+// Skip button
+skipIntro.addEventListener("click", () => {
+    introVideo.pause();
+    intro.style.display = "none";
+    mainWebsite.style.display = "block";
+});
+
+// Search (temporary)
+const searchBox = document.getElementById("searchBox");
+
+if (searchBox) {
+    searchBox.addEventListener("keyup", function () {
+        console.log("Searching:", this.value);
+    });
+}
